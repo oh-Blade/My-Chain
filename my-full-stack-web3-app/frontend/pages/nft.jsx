08098ac49f09639285086235978bf8cefe763a9c
hounaikuo <hounaikuo@canaan-creative.com>
2023-10-10 21:04:56 +0800

    展示指定地址nft

---
+++ my-full-stack-web3-app/frontend/pages/nft.jsx
@@ -0,0 +1,9 @@
+import NftGallery from "../components/nftGallery"
+
+export default function Home() {
+  return (
+    <>
+      <NftGallery walletAddress={"0xe5cB067E90D5Cd1F8052B83562Ae670bA4A211a8"} />
+    </>
+  );
+}
