644a3d5b73d76841572b225d995b7c0c82669807
hounaikuo <hounaikuo@canaan-creative.com>
2023-10-23 09:35:59 +0800

    sign message

---
+++ alchemy-road-to-web3/Ethereum Developer Bootcamp/1.Blockchain Cryptography/recoverPublicKey.js
@@ -0,0 +1,9 @@
+const secp = require("ethereum-cryptography/secp256k1");
+const hashMessage = require("./hashMessage");
+
+async function recoverKey(message, signature, recoveryBit) {
+    return secp.recoverPublicKey(hashMessage(message),signature,recoveryBit);
+    
+}
+
+module.exports = recoverKey;
