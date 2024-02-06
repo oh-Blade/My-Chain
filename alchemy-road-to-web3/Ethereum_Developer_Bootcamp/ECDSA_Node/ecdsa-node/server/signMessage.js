355fbb843b05c61e3ba4b880e1d0e273b17b968d
hounaikuo <hounaikuo@canaan-creative.com>
2024-02-06 17:27:18 +0800

    ecdsa

--- alchemy-road-to-web3/Ethereum_Developer_Bootcamp/ECDSA_Node/ecdsa-node/server/signMessage.js
+++ alchemy-road-to-web3/Ethereum_Developer_Bootcamp/ECDSA_Node/ecdsa-node/server/signMessage.js
@@ -1,10 +1,8 @@
 const secp = require("ethereum-cryptography/secp256k1");
 const hashMessage = require('./hashMessage');
 
-const PRIVATE_KEY = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
-
-async function signMessage(msg) {
-  const si = await secp.sign(hashMessage(msg), PRIVATE_KEY, { lowS:true});
+async function signMessage(msg, key) {
+  const si = await secp.sign(hashMessage(msg), key, { lowS: true });
   console.log(si);
   return si;
 }
